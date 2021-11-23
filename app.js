343a557b74265f45664453e1fb6609e840d50bc1
kimyoungyin <yin199859@gmail.com>
2021-11-24 00:24:26 +0900

    [단계별로 풀어보기] 7단계-1157

--- app.js
+++ app.js
@@ -4,25 +4,25 @@ const str = fs.readFileSync("./.txt").toString().trim().toUpperCase();
 let maxCount = 0;
 let maxStr = "";
 
-for (let i = "A".charCodeAt(0); i < "Z".charCodeAt(0); i++) {
+for (let i = "A".charCodeAt(0); i <= "Z".charCodeAt(0); i++) {
     const al = String.fromCharCode(i);
     let currentIndex = str.indexOf(al);
     let count = 0;
     if (currentIndex !== -1) {
         count = 1;
         while (currentIndex !== -1) {
-            const nextIndex = str.indexOf(al, currentIndex);
-            if (nextIndex !== -1) {
-                currentIndex = nextIndex;
-                count++;
-            }
+            const nextIndex = str.indexOf(al, currentIndex + 1);
+            if (nextIndex === -1) break;
+            currentIndex = nextIndex;
+            count++;
         }
     }
-    console.log(count);
     if (maxCount < count) {
         maxCount = count;
         maxStr = al;
+    } else if (maxCount === count) {
+        maxStr = "?";
     }
 }
 
-// console.log(maxStr);
+console.log(maxStr);
