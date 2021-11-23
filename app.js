17f014df3bb7fd7ff833d3579ef2d1ec54d844a2
kimyoungyin <yin199859@gmail.com>
2021-11-24 00:37:44 +0900

    [단계별로 풀어보기] 7단계-1152

--- app.js
+++ app.js
@@ -1,28 +1,9 @@
 const fs = require("fs");
-const str = fs.readFileSync("./.txt").toString().trim().toUpperCase();
+const strs = fs
+    .readFileSync("./.txt")
+    .toString()
+    .trim()
+    .split(" ")
+    .filter((text) => text !== "");
 
-let maxCount = 0;
-let maxStr = "";
-
-for (let i = "A".charCodeAt(0); i <= "Z".charCodeAt(0); i++) {
-    const al = String.fromCharCode(i);
-    let currentIndex = str.indexOf(al);
-    let count = 0;
-    if (currentIndex !== -1) {
-        count = 1;
-        while (currentIndex !== -1) {
-            const nextIndex = str.indexOf(al, currentIndex + 1);
-            if (nextIndex === -1) break;
-            currentIndex = nextIndex;
-            count++;
-        }
-    }
-    if (maxCount < count) {
-        maxCount = count;
-        maxStr = al;
-    } else if (maxCount === count) {
-        maxStr = "?";
-    }
-}
-
-console.log(maxStr);
+console.log(strs.length);
