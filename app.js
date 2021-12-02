f3406b0973bb0d67efa40b4d2ca87751ed6fb240
kimyoungyin <yin199859@gmail.com>
2021-12-02 20:41:33 +0900

    8단계: 1192

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const fs = require("fs");
-const strs = fs
-    .readFileSync("./.txt")
-    .toString()
-    .trim()
-    .split(" ")
-    .filter((text) => text !== "");
+const num = +fs.readFileSync("./.txt").toString().trim();
 
-console.log(strs.length);
+// 1. 그룹요소 수: 1 + 2 + 3 + 4 증가
+// 2. 그룹 별 분모 분자 합: 그룹 요소 수 + 1
+// 3. 방향: 그룹 요소수가 짝수면 분자가 점점 증가
+
+let groupNum = 1;
+
+const getSum = (groupNum) => (groupNum * (groupNum + 1)) / 2;
+while (true) {
+    if (num - getSum(groupNum) <= 0) {
+        break;
+    }
+    groupNum++;
+}
+const restNum = num - getSum(groupNum - 1);
+console.log(
+    groupNum % 2
+        ? `${groupNum + 1 - restNum}/${restNum}`
+        : `${restNum}/${groupNum + 1 - restNum}`
+);
